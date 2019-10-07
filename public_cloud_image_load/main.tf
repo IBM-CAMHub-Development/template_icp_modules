c893d84b14f60d625e906312a888744fb7310771
CAMHub PublishBot <octravis@us.ibm.com>
2019-10-07 17:35:59 +0000

    Automated CAMHub::publish on 2019-10-07 17:35:58 UTC

---
+++ public_cloud_image_load/main.tf
@@ -0,0 +1,55 @@
+resource "null_resource" "image_copy" {
+  # Only copy image from local location if not available remotely
+  count = "${var.image_location != "" && ! (substr(var.image_location, 0, 3) != "nfs"  || substr(var.image_location, 0, 4) != "http") ? 1 : 0}"
+
+  provisioner "file" {
+    connection {
+      host          = "${ibm_compute_vm_instance.icp-boot.ipv4_address_private}"
+      user          = "icpdeploy"
+      private_key   = "${tls_private_key.installkey.private_key_pem}"
+      bastion_host  = "${var.private_network_only ? ibm_compute_vm_instance.icp-boot.ipv4_address_private : ibm_compute_vm_instance.icp-boot.ipv4_address}"
+    }
+
+    source = "${var.image_location}"
+    destination = "/tmp/${basename(var.image_location)}"
+  }
+}
+
+resource "null_resource" "image_load" {
+  # Only do an image load if we have provided a location. Presumably if not we'll be loading from private registry server
+  count = "${var.image_location != "" ? 1 : 0}"
+  depends_on = ["null_resource.image_copy"]
+
+
+  connection {
+    host          = "${ibm_compute_vm_instance.icp-boot.ipv4_address_private}"
+    user          = "icpdeploy"
+    private_key   = "${tls_private_key.installkey.private_key_pem}"
+    bastion_host  = "${var.private_network_only ? ibm_compute_vm_instance.icp-boot.ipv4_address_private : ibm_compute_vm_instance.icp-boot.ipv4_address}"
+  }
+
+  provisioner "file" {
+    source = "${path.module}/scripts/load_image.sh"
+    destination = "/tmp/load_image.sh"
+  }
+
+  provisioner "remote-exec" {
+
+    # We need to wait for cloud init to finish it's boot sequence.
+    inline = [
+      "while [ ! -f /var/lib/cloud/instance/boot-finished ]; do sleep 1; done",
+      "sudo mv /tmp/load_image.sh /opt/ibm/scripts/",
+      "sudo chmod a+x /opt/ibm/scripts/load_image.sh",
+      "/opt/ibm/scripts/load_image.sh -p ${var.image_location} -r ${local.registry_server} -u ${local.docker_username} -c ${local.docker_password}",
+      "sudo touch /opt/ibm/.imageload_complete"
+    ]
+  }
+}
+
+
+resource "null_resource" "image_loading_finished" {
+  depends_on = ["null_resource.image_load"]
+  provisioner "local-exec" {
+    command = "echo 'IBM Cloud Private Image has been successfully loaded. "
+  }
+}
